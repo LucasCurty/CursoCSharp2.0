c03d9b623a990b013765559935d4e94525fb8e67
Lucas <lukas.curty.21@gmail.com>
2024-11-02 13:11:49 -0300

    cirando classes

--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -12,7 +12,9 @@ namespace PrimeiroPrograma
             //leituraEntrada();
             //Maior(3, 5, 7);
             //conferirNegativo();
-            enquanto();
+            //estruturaWhile();
+            //estruturaFor();
+            areaTriangulo();
         }
         static void conferirSePar(int numero)
         {
@@ -26,37 +28,37 @@ namespace PrimeiroPrograma
             }
             else
             {
-               Console.WriteLine("numero é impar!");
+                Console.WriteLine("numero é impar!");
             }
 
         }
         static void basicoVariaveis()
         {
-          
-          bool completo = false;
-           char sexo = 'A';
-           string nome = "Lucas";
 
-           object obj1 = "Leeo";
-           double obj2 = 888.656516;
+            bool completo = false;
+            char sexo = 'A';
+            string nome = "Lucas";
+
+            object obj1 = "Leeo";
+            double obj2 = 888.656516;
 
 
-           int n1 = 127;
-           long n2 = 561561651651561651L;
-           float n3 = 1.4f;
-           double n4 = 4.5;
+            int n1 = 127;
+            long n2 = 561561651651561651L;
+            float n3 = 1.4f;
+            double n4 = 4.5;
 
-           Console.WriteLine(n1);
-           Console.WriteLine(n2);
-           Console.WriteLine(n4);
-           Console.WriteLine(n3);
-           Console.WriteLine(completo);
-           Console.WriteLine(sexo);
-           Console.WriteLine(nome);
-           Console.WriteLine(obj1);
-           Console.WriteLine(obj2.ToString("F2"));
-           Console.WriteLine($"{nome} esta estudando CSharp, com uma porcentagem de  {obj2}%");
-          
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n4);
+            Console.WriteLine(n3);
+            Console.WriteLine(completo);
+            Console.WriteLine(sexo);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2.ToString("F2"));
+            Console.WriteLine($"{nome} esta estudando CSharp, com uma porcentagem de  {obj2}%");
+
         }
 
         static void leituraEntrada()
@@ -65,7 +67,7 @@ namespace PrimeiroPrograma
             // teste de leitura do input
 
 
-           Console.WriteLine("digite o primeiro numero!");
+            Console.WriteLine("digite o primeiro numero!");
             int valor1 = int.Parse(Console.ReadLine());
             Console.WriteLine("digite o segundo numero!");
             int valor2 = int.Parse(Console.ReadLine());
@@ -93,7 +95,7 @@ namespace PrimeiroPrograma
 
         static void conferirNegativo()
         {
-            int numero = int.Parse (Console.ReadLine());
+            int numero = int.Parse(Console.ReadLine());
 
             if (numero < 0)
             {
@@ -105,7 +107,7 @@ namespace PrimeiroPrograma
             }
         }
 
-        static void enquanto()
+        static void estruturaWhile()
         {
             Console.WriteLine("Digite um número: ");
             double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -118,6 +120,58 @@ namespace PrimeiroPrograma
                 x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             }
         }
+
+        static void estruturaFor()
+        {
+            Console.Write("Quantos numeros inteiros voce ira digitar? ");
+            int N = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Valor #{i}");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+
+        static void areaTriangulo()
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine($"Area de X = {areaX}");
+            Console.WriteLine($"Area de Y = {areaY}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Mario area: Y");
+                }
+
+        }
     }
-    
+
 }
---
+++ PrimeiroPrograma/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrimeiroPrograma
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
