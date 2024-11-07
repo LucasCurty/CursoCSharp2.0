04a28f9853049a2eacf8e09436c618fb295a4d2f
Lucas <lukas.curty.21@gmail.com>
2024-11-06 21:02:31 -0300

    Arrumando os dados da Class Conta

--- PrimeiroPrograma/Conta.cs
+++ PrimeiroPrograma/Conta.cs
@@ -4,16 +4,14 @@
         public int NumeroConta {  get; private set; }
         public string Titular { get; private set; }
         public double Saldo { get; private set; }
-        private readonly double Taxa = 5.00;
+      
 
         public Conta(string titular, int numeroConta) {
             Titular = titular;
             NumeroConta = numeroConta;
         }
-        public Conta(string titular, int numeroConta, double saldo) : this() {
-            Titular = titular;
-            NumeroConta = numeroConta;
-            Deposito(saldo);
+        public Conta(string titular, int numeroConta, double saldo) : this(titular, numeroConta) {
+            Saldo = saldo;
         }
 
 
@@ -22,7 +20,7 @@
         }
 
         public double Saque(double saque) {
-            return Saldo - saque - Taxa;
+            return Saldo - saque + 5.00;
         }
 
 
